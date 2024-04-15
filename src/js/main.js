a2d3029f11340af8a56881669464a2d69d5f680b
Eng-ali28 <eng.ali10203@gmail.com>
2024-04-15 20:57:17 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log('Hello Crack Head');
+// start play/stop btn
+const playBtn = document.getElementById('playbtn');
+
+const playBtnOnClick = (e) => {
+    if (playBtn.innerText == 'Stop') playBtn.innerText = 'Play';
+    else if (playBtn.innerText == 'Play') playBtn.innerText = 'Stop';
+};
+
+playBtn.onclick = playBtnOnClick;
+// end play/stop btn
